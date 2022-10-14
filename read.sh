53f0933912f8afa128e15bfc79c8be6a467203cb
“eda” <acikgozeda8588@gmail.com>
2022-10-14 21:27:11 +0300

    added

---
+++ read.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p  " dosya ismini giriniz: " dosyaismi
+
+if [[ -f $dosyaismi ]]
+then
+   if [[ -w $dosyaismi ]]
+   then
+      echo " yazma işlemi bitince ctrl+d ile cıkabilirsiniz"
+      cat >> dosyaismi
+
+   else
+       echo "dosya yazılabilir değil"
+   fi
+else 
+    echo "dosya mevcut değil"
+
+fi  
